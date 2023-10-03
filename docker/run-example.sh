4b3d79e3a13601bc5b07b5a1a2f4edb78008c5e0
Lorenzo Drudi <lori.drudi.11@gmail.com>
2023-10-03 10:25:57 +0200

    init: improve docker structure.

--- docker/run-example.sh
+++ docker/run-example.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run  -it da_image /bin/bash 
+# The first parameter is the name of the local directory that contains the
+# source code of the project. You must specify the local absolute path.
+
+docker run \
+-v /home/drudao/Desktop/EPFL/Year_I/DistributedAlgorithms/Project/distributed-algorithms/java:/root/java \
+-w /root/java \
+-it da_image /bin/bash 
