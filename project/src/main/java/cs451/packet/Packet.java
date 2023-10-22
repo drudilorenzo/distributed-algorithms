fc6ed4aa0a10d9615101da15e00a393131f875c6
Lorenzo Drudi <lori.drudi.11@gmail.com>
2023-10-22 14:21:02 +0200

    Create `packet` interface.

--- project/src/main/java/cs451/packet/Packet.java
+++ project/src/main/java/cs451/packet/Packet.java
@@ -1,15 +1,69 @@
 package cs451.packet;
 
-public interface Packet {
+import java.io.Serializable;
+
+/**
+ * Interface for UDP Packets.
+ */
+public interface Packet extends Serializable {
+
 
     public static final int MAX_PAYLOAD_SIZE = 65507;
 
-    byte[] getData();
-
+    /**
+     * Get the packet id.
+     *
+     * @return the id of the packet.
+     */
     int getPacketId();
 
-    int getTimestamp();
+    /**
+     * Get the length of the payload.
+     *
+     * @return The length of the payload.
+     */
+    int getLength();
 
+    /**
+     * Check if a packet is an ack packet.
+     *
+     * @return True if it is an packet, false otherwise.
+     */
     boolean isAck();
+
+    /**
+     * Get the id of the sender.
+     *
+     * @return The id of the sender.
+     */
+    int getSenderId();
+
+    /**
+     * Get the id of the receiver.
+     *
+     * @return The id of the receiver.
+     */
+    int getReceiverId();
+
+    /**
+     * Get the packet data as a byte array.
+     *
+     * @return A byte array containing the packet data.
+     */
+    byte[] getPacketInBytes();
+
+    /**
+     * Get the payload of the packet.
+     *
+     * @return A byte array containing the payload.
+     */
+    byte[] getPayload();
+
+    /**
+     * Get the ack packet of the current one.
+     *
+     * @return The ack packet.
+     */
+    Packet toAck();
 
 }
