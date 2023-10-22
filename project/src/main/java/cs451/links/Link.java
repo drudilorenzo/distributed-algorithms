3a39e64f040d8440ab4f8fbeba726437facfe88d
Lorenzo Drudi <lori.drudi.11@gmail.com>
2023-10-22 14:21:39 +0200

    pl: Add link interface.

--- project/src/main/java/cs451/links/Link.java
+++ project/src/main/java/cs451/links/Link.java
@@ -1,10 +1,25 @@
 package cs451.links;
 
-import cs451.Host;
 import cs451.packet.Packet;
 
-public interface Link {
+/**
+ * Interface for the abstraction of a link (used to represent the network components of the
+ * distributed system).
+ *
+ * Every pair of processes is connected by a bidirectional link.
+ */
+interface Link {
 
-    void send(Packet packet, Host host);
+    /**
+     * Send a packet.
+     *
+     * @param packet: the packet to send.
+     */
+    void send(Packet packet);
+
+    /**
+     * Close the link.
+     */
+    void close();
 
 }
